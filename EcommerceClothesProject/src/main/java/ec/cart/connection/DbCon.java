7dba35c836521a2eb653977c00525cdd8e9ef194
ethanimperial <2001200A@student.tp.edu.sg>
2022-02-05 08:01:59 +0800

    created cart page with database connection

---
+++ EcommerceClothesProject/src/main/java/ec/cart/connection/DbCon.java
@@ -0,0 +1,17 @@
+package ec.cart.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbCon {
+	private static Connection connection = null;
+	public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        if(connection == null){
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerceproject","root","password");
+            System.out.print("connected");
+        }
+        return connection;
+    }
+}
