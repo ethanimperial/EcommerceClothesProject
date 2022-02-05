e4e8d5b95b88a279ca167cbe383133bd66ec701e
Dylan <2001801F@student.tp.edu.sg>
2022-02-05 18:38:33 +0800

    Added login.jsp, edited LoginServlet to compare form with database

--- EcommerceClothesProject/src/main/java/cn/techtutorial/servlet/LoginServlet.java
+++ EcommerceClothesProject/src/main/java/cn/techtutorial/servlet/LoginServlet.java
@@ -2,12 +2,17 @@ package cn.techtutorial.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import cn.techtutorial.connection.DBCon;
+import cn.techtutorial.dao.UserDao;
+import cn.techtutorial.model.User;
 
 /**
  * Servlet implementation class LoginServlet
@@ -26,7 +31,20 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
 		try(PrintWriter out= response.getWriter()){
-			out.print("this is login servlet");
+			String email = request.getParameter("login-email");
+			String password = request.getParameter("login-password");
+
+			UserDao udao = new UserDao(DBCon.getConnection());
+			User user = udao.userLogin(email, password);
+			if (user != null) {
+				request.getSession().setAttribute("auth", user);
+//				System.out.print("user logged in");
+				response.sendRedirect("index.jsp");
+			} else {
+				out.println("Your username or password is incorrect, please try again.");
+			}
+		} catch (ClassNotFoundException|SQLException e) {
+			e.printStackTrace();
 		}
 	}
 
