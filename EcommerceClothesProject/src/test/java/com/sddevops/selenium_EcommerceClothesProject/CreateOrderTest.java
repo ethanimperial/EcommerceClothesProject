78ccae905bd37e1ea3607ccc5fc19dee21b04b69
ethanimperial <2001200A@student.tp.edu.sg>
2022-02-24 20:34:28 +0800

    updated selenium testing files

--- EcommerceClothesProject/src/test/java/com/sddevops/selenium_EcommerceClothesProject/CreateOrderTest.java
+++ EcommerceClothesProject/src/test/java/com/sddevops/selenium_EcommerceClothesProject/CreateOrderTest.java
@@ -10,6 +10,8 @@ import org.testng.annotations.AfterTest;
 
 public class CreateOrderTest {
 	
+	 private WebDriver webDriver;		
+	
   @Test
   public void checkInput() {
 	  System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
@@ -42,10 +44,22 @@ public class CreateOrderTest {
   }
   @BeforeTest
   public void beforeTest() {
+	  
+	  //Setting system properties of ChromeDriver
+	  //to amend directory path base on your local file path
+	  String chromeDriverDir = "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe";
+
+	  System.setProperty("webdriver.chrome.driver", chromeDriverDir);
+
+	  //initialize FirefoxDriver at the start of test
+	  webDriver = new ChromeDriver();  
   }
 
   @AfterTest
   public void afterTest() {
+	  
+	  //Quit the ChromeDriver and close all associated window at the end of test
+	  webDriver.quit();	
   }
 
 }
