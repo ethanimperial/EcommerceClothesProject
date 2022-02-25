a03fa7f39692c9f041482f9614e36dd8b435b76e
ethanimperial <2001200A@student.tp.edu.sg>
2022-02-25 12:12:03 +0800

    updated selenium testing

---
+++ EcommerceClothesProject/src/test/java/com/sddevops/EcommerceClothesProject/CreateOrderTest.java
@@ -0,0 +1,66 @@
+package com.sddevops.EcommerceClothesProject;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class CreateOrderTest {
+	
+	 private WebDriver webDriver;		
+	
+  @Test
+  public void checkInput() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+      WebDriver driver=new ChromeDriver();
+      
+      driver.get("http://localhost:8090/EcommerceClothesProject/AddToCartServlet?id=60");
+      driver.get("http://localhost:8090/EcommerceClothesProject/AddToCartServlet?id=61");
+      driver.get("http://localhost:8090/EcommerceClothesProject/AddToCartServlet?id=62");
+      
+      driver.get("http://localhost:8090/EcommerceClothesProject/cart.jsp");
+      
+      
+  
+      driver.get("http://localhost:8090/EcommerceClothesProject/QuantityServlet?action=inc&id=60");
+      
+      driver.get("http://localhost:8090/EcommerceClothesProject/QuantityServlet?action=inc&id=60");
+      
+      driver.get("http://localhost:8090/EcommerceClothesProject/QuantityServlet?action=dec&id=60");
+      
+      
+
+      driver.get("http://localhost:8090/EcommerceClothesProject/RemoveFromCartServlet?id=61");
+    
+     
+      WebElement check = driver.findElement(By.name("check"));
+      
+      check.click();
+      
+      
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  
+	  //Setting system properties of ChromeDriver
+	  //to amend directory path base on your local file path
+	  String chromeDriverDir = "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe";
+
+	  System.setProperty("webdriver.chrome.driver", chromeDriverDir);
+
+	  //initialize FirefoxDriver at the start of test
+	  webDriver = new ChromeDriver();  
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  
+	  //Quit the ChromeDriver and close all associated window at the end of test
+	  webDriver.quit();	
+  }
+
+}
+
